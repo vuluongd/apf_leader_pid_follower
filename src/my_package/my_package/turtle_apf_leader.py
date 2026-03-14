d41b51508af462dd86fa6a60b20053affd18b42f
vuluongd <vul45845@gmail.com>
2026-03-15 00:21:49 +0700

    third commit

--- src/my_package/my_package/turtle_apf_leader.py
+++ src/my_package/my_package/turtle_apf_leader.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from turtlesim.msg import Pose
 from geometry_msgs import Twist
 import math
+import random
 
 class APFLeader(Node):
     def __init__(self):
@@ -38,7 +39,58 @@ class APFLeader(Node):
                 10,
             ) 
 
+        #publisher+timer
+        self.pub = self.create_publisher(Twist, 'turtle1/cmd_vel/', 10)
+        self.create_timer(0.05, self.control_loop)
+
+
+        @staticmethod
+        def random_goal(margin, float = 1.0) -> tuple[float, float]:
+            return (
+                random.uniform(margin, 11.0 - margin),
+                random.uniform(margin, 11.0 - margin)
+            )
+        #APF core
+        def attractive_force(self) -> tuple[float, float]:
+            ka = self.get_parameters('ka').value
+            return ka*(self.goal_x - self.pose.x), ka*(self.goal_y - self.pose.y)
         
+        def repulsive_force(self) -> tuple[float, float]:
+            kr = self.get_parameters('kr').value
+            rho_0 = self.get_parameters('rho_0').value
+            fx = 0
+            fy = 0
+
+            for obstacle in self.obstacle_poses.values():
+                dx = self.pose.x - obstacle.x
+                dy = self.pose.y - obstacle.y
+                rho = math.hypot(dx, dy)
+                if 0.0 < rho < rho_0:
+                    coef = kr*(1.0/rho - 1.0/rho_0)/rho ** 2
+                    fx += coef * dx/rho
+                    fy += coef * dy/rho
+
+            return fx, fy
+        
+        def leader_cb(self, msg: Pose):
+            self.pose = msg
+
+        def control_loop(self):
+            if self.pose is None:
+                return
+            
+            goal_radius = self.get_parameter('goal_radius').value()
+            max_linear = self.get_parameter('max_linear').value()
+            max_angular = self.get_parameter('max_angular').value()
+            kp_angular = self.get_parameter('kp_angular').value()
+
+            
+        
+
+
+        
+
+
 
 
 
